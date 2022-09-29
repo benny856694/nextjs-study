a8a2331801ba4cbc803fa4ea0d0d56c91f95826e
Benny Shen <shenbinsc@qq.com>
2022-09-29 23:33:31 +0800

    add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,11 @@ export default function Home({ products }: InferGetStaticPropsType<typeof getSta
   return (
     <div>
       <Head>
-        <title>Fake Ecommerce</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <title>Ecommerce Shop</title>
       </Head>
 
       <main className="">
