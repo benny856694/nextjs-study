780fecedb3c3c35a11ae3aa54ed9d0d7123f8d43
Benny Shen <shenbinsc@qq.com>
2022-09-30 21:46:18 +0800

    cosmetic

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -33,9 +33,9 @@ export const getStaticProps: GetStaticProps<{ product: Product }> = async ({ par
 
 
 export default function ProductPage({ product }: InferGetStaticPropsType<typeof getStaticProps>) {
-    return <div className='flex items-start justify-center container mx-auto p-4 max-w-4xl' key={product.id}>
+    return <div className='flex flex-col items-center md:flex-row md:items-start md:justify-center container mx-auto p-4 max-w-4xl' key={product.id}>
         <Image src={product.image} width={300} height={50} alt={''} />
-        <div className='flex flex-col ml-10 content-start items-start'>
+        <div className='flex flex-col md:ml-10 mt-8 md:mt-0 content-start items-start'>
             <h4 className='font-bold leading-tight'>{product.title}</h4>
             <h5 className='font-medium my-2'>{product.category}</h5>
             <p className='mt-8'>{product.description}</p>
