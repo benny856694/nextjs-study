d341fc2ff01a7dde7213435491dc90e5b89420cc
BinShen <shenbinsc@gmail.com>
2022-09-30 16:53:58 +0800

    cosmetic

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,46 +6,19 @@ import type { Product } from './model/product';
 
 
 
-export const getStaticProps: GetStaticProps<{ products: Product[] }> = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
   const resp = await fetch(`${baseUrl}/products`)
   const json = await resp.json() as Product[]
   console.log(json)
   return {
-    props: {
-      products: json
-    }
+    redirect: { statusCode: 302, destination: '/products' }
   }
 }
 
 export default function Home({ products }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div>
-      <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <title>Ecommerce Shop</title>
-      </Head>
-
-      <main className="">
-        <div className="text-lg sticky top-0 bg-black text-white font-bold p-2">
-          <h1 className="container mx-auto">Ecommerce Shop</h1>
-        </div>
-        <div className='container mx-auto flex flex-wrap p-4 gap-2 justify-center'>
-          {
-            products.map(p => (<a href='#' className='flex w-96 items-start border rounded-xl p-4 hover:shadow-lg transition' key={p.id}>
-              <Image src={p.image} width={100} height={50} alt={''} />
-              <div className='flex flex-col ml-4 content-start items-start'>
-                <h4 className='font-bold leading-tight'>{p.title}</h4>
-                <h5 className='font-medium my-2'>{p.category}</h5>
-                <p className='line-clamp-3 break-all'>{p.description}</p>
-              </div>
-
-            </a>))
-          }
-        </div>
-      </main>
+      redirected
     </div>
   );
 }
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -35,10 +35,10 @@ export const getStaticProps: GetStaticProps<{ product: Product }> = async ({ par
 export default function ProductPage({ product }: InferGetStaticPropsType<typeof getStaticProps>) {
     return <div className='flex items-start justify-center container mx-auto p-4 max-w-4xl' key={product.id}>
         <Image src={product.image} width={300} height={50} alt={''} />
-        <div className='flex flex-col ml-8 content-start items-start'>
+        <div className='flex flex-col ml-10 content-start items-start'>
             <h4 className='font-bold leading-tight'>{product.title}</h4>
             <h5 className='font-medium my-2'>{product.category}</h5>
-            <p className=''>{product.description}</p>
+            <p className='mt-8'>{product.description}</p>
         </div>
 
     </div>
