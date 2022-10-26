162a4caacbc7864f63ff6eab0aa923f8599e0190
benny856694 <shenbinsc@gmail.com>
2022-10-26 16:06:08 +0800

    fix tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
+import '../styles/globals.css';
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
+      <title>NextJs 13</title>
       <head></head>
-      <body>{children}</body>
+      <body className="container mx-auto">
+        <h2 className="text-center text-lg mt-4">Nextjs 13 is Cool</h2>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
