2bf42fb1a6fdb6fe3a306b9acb2e76fb4032b7f3
Benny Shen <shenbinsc@qq.com>
2022-11-06 13:38:35 +0800

    cosmetic

--- app/page.tsx
+++ app/page.tsx
@@ -18,16 +18,15 @@ export default async function Page() {
       <div className="text-lg sticky top-0 bg-black/50 backdrop-blur-sm text-white font-bold p-2">
         <h1 className="container mx-auto">Ecommerce Shop</h1>
       </div>
-      <div className="container mx-auto flex flex-wrap p-4 gap-2 justify-center">
+      <div className="container mx-auto flex flex-wrap p-4 gap-2 justify-center items-stretch">
         {products.map((p) => (
-          <Link href={`/products/${encodeURIComponent(p.id)}`} key={p.id}>
-            <div className="flex w-96 items-start border rounded-xl p-4 hover:shadow-lg transition cursor-pointer">
-              <Image src={p.image} width={100} height={50} alt={''} />
-              <div className="flex flex-col ml-4 content-start items-start">
-                <h4 className="font-bold leading-tight">{p.title}</h4>
-                <h5 className="font-medium my-2">{p.category}</h5>
-                <p className="line-clamp-3 break-all">{p.description}</p>
-              </div>
+          <Link className="border border-gray-300 flex w-96 items-stretch rounded-xl p-4 hover:shadow-lg transition cursor-pointer"
+            href={`/products/${encodeURIComponent(p.id)}`} key={p.id}>
+            <Image className="self-start" src={p.image} width={100} height={50} alt={''} />
+            <div className="flex flex-col ml-4 content-start items-start">
+              <h4 className="font-bold leading-tight">{p.title}</h4>
+              <h5 className="font-medium my-2">{p.category}</h5>
+              <p className="line-clamp-3 break-all">{p.description}</p>
             </div>
           </Link>
         ))}
